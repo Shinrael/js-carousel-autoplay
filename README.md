7534c0c68d6880c1066a1b5e87692aadfee68064
Shinrael <mircoelena89@gmail.com>
2024-03-12 15:18:43 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-js-carousel-autoplay
+vue-slider
 ===
-Riprendendo il carousel realizzato in passato  e con le Timeng Function rendete lo scroll automatico ogni 3 secondi.
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 ### Svolgimento
-1. Riprendiamo il codice fatto in precedenza e trasformo l'evento per procedere avanti con la slide in una function (che in precedenza non avevamo ancora studiato).
-2. creo un setInterval di 3 secondi dandogli la funzione per muoversi in avanti.
+1. Tramite v-for mettiamo le immagini dentro lo slider
--- js/script.js
+++ js/script.js
@@ -1,112 +1,21 @@
-// Creo la const con il contenitore delle immagini e i pulsanti
+const { createApp } =  Vue;
+createApp({
+  data(){
+    return{
+      images: [
+        '..\assets\img\01.webp',
+        '..\assets\img\02.webp',
+        '..\assets\img\03.webp',
+        '..\assets\img\04.webp',
+        '..\assets\img\05.webp',
+      ]
+    }
+  },
+  methods:{
 
-const imageContainer = document.querySelector('.image-container');
-const up = document.querySelector('.up');
-const down = document.querySelector('.down');
-const cardsContainer = document.querySelector('.card-lat');
-let intervalId;
+  },
+  mounted(){
+    
+  }
+}).mount('#app')
 
-let counterImg = 0;
-
-// Creo l'array con tutte le immagini
-
-const images = [
-  'assets/img/01.webp',
-  'assets/img/02.webp',
-  'assets/img/03.webp',
-  'assets/img/04.webp',
-  'assets/img/05.webp',
-]
-
-// Avvio il ciclo per aggiungere la classe alle immagini
-
-for (let i = 0; i < images.length; i++) {
-  const img = images[i];
-  imageContainer.innerHTML += `<img class="image hidden" src="${img}">`
-  cardsContainer.innerHTML += ` <div class="card-container">
-  <img src="${img}"> 
-  `
-}
-
-// Prendiamo tutte le immagini
-
-const imgCollection = document.getElementsByClassName('image');
-const cardCollection = document.getElementsByClassName('card-container');
-
-
-// Rimuoviamo la classe hidden alla prima immagine
-
-
-imgCollection[0].classList.remove('hidden');
-
-// BONUS 2 aggiungo la classe .active-card alla prima card
-
-cardCollection[counterImg].classList.add('active-card');
-
-// Incrementiamo e decrementiamo il counter con le frecce
-
-up.addEventListener('click', function (){
-   // Prima aggiungo la classe hidden
-
-   imgCollection[counterImg].classList.add('hidden');
-
-   //  BONUS 2
-
-   cardCollection[counterImg].classList.remove('active-card');
-
-  //  BONUS 1
-
-   counterImg = (counterImg - 1 + images.length) % images.length;
-
-
-   // Dopo la rimuovo
- 
-   imgCollection[counterImg].classList.remove('hidden');
-
-   cardCollection[counterImg].classList.add('active-card');
-})
-
-down.addEventListener('click', function (){  
-
-  goAhead();
-
-})
-
-// Creo l'evento per settare uno stop quando vado sopra al cardsContainer
-
-cardsContainer.addEventListener('mouseover', function() {
-  clearInterval(intervalId);
-});
-
-// Creo l'evento per far avanzare automaticamente lo slider quando il mouse sta fuori dal cardsContainer
-
-cardsContainer.addEventListener('mouseout', function() {
-  intervalId = setInterval(goAhead, 3000);
-});
-
-
-
-//////////////////////////////////// FUNCTION
-
-function goAhead() {
-
-  // Prima aggiungo la classe hidden
-
-  imgCollection[counterImg].classList.add('hidden');
-
-  // BONUS 2
-
-  cardCollection[counterImg].classList.remove('active-card');
-
-
-  // BONUS 1
- 
-  counterImg = (counterImg + 1) % images.length;
-
-
-  // Dopo la rimuovo
-
-  imgCollection[counterImg].classList.remove('hidden');
-
-  cardCollection[counterImg].classList.add('active-card');
-}
